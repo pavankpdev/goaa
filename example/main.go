f015f172ae57c24440298f6715c79104a0e6764c
pavanleton <pavankumarkpkp@gmail.com>
2023-10-11 14:39:43 +0530

    chore(example): update example to fetch and print SA address for the given salt

--- example/main.go
+++ example/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/pavankpdev/goaa"
 )
 
 func main() {
-	var client = goaa.CreateClient("https://eth-sepolia.g.alchemy.com/v2/3QDeRBQwnvG0DyXXVyFds7I7w0f8dUfe")
-	fmt.Println(client.ChainID(context.Background()))
-	fmt.Println(goaa.PrivateKeyToAddress("af5ead4413ff4b78bc94191a2926ae9ccbec86ce099d65aaf469e9eb1a0fa87f"))
+
+	const RPC = "<RPC>"
+	const SmartAccountFactoryAddress = "0x9406Cc6185a346906296840746125a0E44976454" // https://docs.alchemy.com/docs/creating-a-smart-contract-account-and-sending-userops#1b-get-constants
+	const PrivateKey = "<PrivateKey>"
+
+	params := goaa.SmartAccountProviderParams{
+		OwnerPrivateKey:            PrivateKey,
+		RPC:                        RPC,
+		EntryPointAddress:          "",
+		SmartAccountFactoryAddress: SmartAccountFactoryAddress,
+	}
+
+	client, err := goaa.NewSmartAccountProvider(params)
+
+	if err != nil {
+		panic(err)
+	}
+
+	address, err := client.GetSmartAccountAddress(1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("My samrt account address is %v\n", address)
+
 }
