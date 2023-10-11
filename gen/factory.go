5f276a6bb7e7b6645059a35778bbf4abb4b9821e
pavanleton <pavankumarkpkp@gmail.com>
2023-10-11 14:25:21 +0530

    update: setup `SmartAccountProvider` to use alchemy's smart account factory contract

---
+++ gen/factory.go
@@ -0,0 +1,264 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package gen
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+	_ = abi.ConvertType
+)
+
+// FactoryMetaData contains all meta data concerning the Factory contract.
+var FactoryMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[{\"internalType\":\"contractIEntryPoint\",\"name\":\"_entryPoint\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"inputs\":[],\"name\":\"accountImplementation\",\"outputs\":[{\"internalType\":\"contractSimpleAccount\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"salt\",\"type\":\"uint256\"}],\"name\":\"createAccount\",\"outputs\":[{\"internalType\":\"contractSimpleAccount\",\"name\":\"ret\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"salt\",\"type\":\"uint256\"}],\"name\":\"getAddress\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
+}
+
+// FactoryABI is the input ABI used to generate the binding from.
+// Deprecated: Use FactoryMetaData.ABI instead.
+var FactoryABI = FactoryMetaData.ABI
+
+// Factory is an auto generated Go binding around an Ethereum contract.
+type Factory struct {
+	FactoryCaller     // Read-only binding to the contract
+	FactoryTransactor // Write-only binding to the contract
+	FactoryFilterer   // Log filterer for contract events
+}
+
+// FactoryCaller is an auto generated read-only Go binding around an Ethereum contract.
+type FactoryCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// FactoryTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type FactoryTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// FactoryFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type FactoryFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// FactorySession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type FactorySession struct {
+	Contract     *Factory          // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// FactoryCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type FactoryCallerSession struct {
+	Contract *FactoryCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts  // Call options to use throughout this session
+}
+
+// FactoryTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type FactoryTransactorSession struct {
+	Contract     *FactoryTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts  // Transaction auth options to use throughout this session
+}
+
+// FactoryRaw is an auto generated low-level Go binding around an Ethereum contract.
+type FactoryRaw struct {
+	Contract *Factory // Generic contract binding to access the raw methods on
+}
+
+// FactoryCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type FactoryCallerRaw struct {
+	Contract *FactoryCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// FactoryTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type FactoryTransactorRaw struct {
+	Contract *FactoryTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewFactory creates a new instance of Factory, bound to a specific deployed contract.
+func NewFactory(address common.Address, backend bind.ContractBackend) (*Factory, error) {
+	contract, err := bindFactory(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Factory{FactoryCaller: FactoryCaller{contract: contract}, FactoryTransactor: FactoryTransactor{contract: contract}, FactoryFilterer: FactoryFilterer{contract: contract}}, nil
+}
+
+// NewFactoryCaller creates a new read-only instance of Factory, bound to a specific deployed contract.
+func NewFactoryCaller(address common.Address, caller bind.ContractCaller) (*FactoryCaller, error) {
+	contract, err := bindFactory(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &FactoryCaller{contract: contract}, nil
+}
+
+// NewFactoryTransactor creates a new write-only instance of Factory, bound to a specific deployed contract.
+func NewFactoryTransactor(address common.Address, transactor bind.ContractTransactor) (*FactoryTransactor, error) {
+	contract, err := bindFactory(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &FactoryTransactor{contract: contract}, nil
+}
+
+// NewFactoryFilterer creates a new log filterer instance of Factory, bound to a specific deployed contract.
+func NewFactoryFilterer(address common.Address, filterer bind.ContractFilterer) (*FactoryFilterer, error) {
+	contract, err := bindFactory(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &FactoryFilterer{contract: contract}, nil
+}
+
+// bindFactory binds a generic wrapper to an already deployed contract.
+func bindFactory(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := FactoryMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Factory *FactoryRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Factory.Contract.FactoryCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Factory *FactoryRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Factory.Contract.FactoryTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Factory *FactoryRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Factory.Contract.FactoryTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Factory *FactoryCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Factory.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Factory *FactoryTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Factory.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Factory *FactoryTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Factory.Contract.contract.Transact(opts, method, params...)
+}
+
+// AccountImplementation is a free data retrieval call binding the contract method 0x11464fbe.
+//
+// Solidity: function accountImplementation() view returns(address)
+func (_Factory *FactoryCaller) AccountImplementation(opts *bind.CallOpts) (common.Address, error) {
+	var out []interface{}
+	err := _Factory.contract.Call(opts, &out, "accountImplementation")
+
+	if err != nil {
+		return *new(common.Address), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(common.Address)).(*common.Address)
+
+	return out0, err
+
+}
+
+// AccountImplementation is a free data retrieval call binding the contract method 0x11464fbe.
+//
+// Solidity: function accountImplementation() view returns(address)
+func (_Factory *FactorySession) AccountImplementation() (common.Address, error) {
+	return _Factory.Contract.AccountImplementation(&_Factory.CallOpts)
+}
+
+// AccountImplementation is a free data retrieval call binding the contract method 0x11464fbe.
+//
+// Solidity: function accountImplementation() view returns(address)
+func (_Factory *FactoryCallerSession) AccountImplementation() (common.Address, error) {
+	return _Factory.Contract.AccountImplementation(&_Factory.CallOpts)
+}
+
+// GetAddress is a free data retrieval call binding the contract method 0x8cb84e18.
+//
+// Solidity: function getAddress(address owner, uint256 salt) view returns(address)
+func (_Factory *FactoryCaller) GetAddress(opts *bind.CallOpts, owner common.Address, salt *big.Int) (common.Address, error) {
+	var out []interface{}
+	err := _Factory.contract.Call(opts, &out, "getAddress", owner, salt)
+
+	if err != nil {
+		return *new(common.Address), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(common.Address)).(*common.Address)
+
+	return out0, err
+
+}
+
+// GetAddress is a free data retrieval call binding the contract method 0x8cb84e18.
+//
+// Solidity: function getAddress(address owner, uint256 salt) view returns(address)
+func (_Factory *FactorySession) GetAddress(owner common.Address, salt *big.Int) (common.Address, error) {
+	return _Factory.Contract.GetAddress(&_Factory.CallOpts, owner, salt)
+}
+
+// GetAddress is a free data retrieval call binding the contract method 0x8cb84e18.
+//
+// Solidity: function getAddress(address owner, uint256 salt) view returns(address)
+func (_Factory *FactoryCallerSession) GetAddress(owner common.Address, salt *big.Int) (common.Address, error) {
+	return _Factory.Contract.GetAddress(&_Factory.CallOpts, owner, salt)
+}
+
+// CreateAccount is a paid mutator transaction binding the contract method 0x5fbfb9cf.
+//
+// Solidity: function createAccount(address owner, uint256 salt) returns(address ret)
+func (_Factory *FactoryTransactor) CreateAccount(opts *bind.TransactOpts, owner common.Address, salt *big.Int) (*types.Transaction, error) {
+	return _Factory.contract.Transact(opts, "createAccount", owner, salt)
+}
+
+// CreateAccount is a paid mutator transaction binding the contract method 0x5fbfb9cf.
+//
+// Solidity: function createAccount(address owner, uint256 salt) returns(address ret)
+func (_Factory *FactorySession) CreateAccount(owner common.Address, salt *big.Int) (*types.Transaction, error) {
+	return _Factory.Contract.CreateAccount(&_Factory.TransactOpts, owner, salt)
+}
+
+// CreateAccount is a paid mutator transaction binding the contract method 0x5fbfb9cf.
+//
+// Solidity: function createAccount(address owner, uint256 salt) returns(address ret)
+func (_Factory *FactoryTransactorSession) CreateAccount(owner common.Address, salt *big.Int) (*types.Transaction, error) {
+	return _Factory.Contract.CreateAccount(&_Factory.TransactOpts, owner, salt)
+}
--- goaa.go
+++ goaa.go
@@ -1,41 +1,83 @@
 package goaa
 
 import (
-	"crypto/ecdsa"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	factory "github.com/pavankpdev/goaa/gen"
+	"math/big"
 )
 
-// CreateClient connects to an Ethereum node via the specified RPC endpoint
+// SmartAccountProviderParams stores the parameters required to initialize the SmartAccountProvider.
+type SmartAccountProviderParams struct {
+	OwnerPrivateKey            string // The private key of the Ethereum account
+	RPC                        string // The RPC endpoint for the Ethereum node
+	EntryPointAddress          string // The address of the entry point contract
+	SmartAccountFactoryAddress string // The address of the smart account factory contract
+}
+
+// SmartAccountProvider is a struct that manages interaction with Ethereum smart contracts.
+type SmartAccountProvider struct {
+	Client    *ethclient.Client // Ethereum client for interacting with the blockchain
+	Owner     common.Address    // Ethereum address of the owner
+	SAFactory *factory.Factory  // Smart account factory contract instance
+}
+
+// NewSmartAccountProvider creates a new instance of SmartAccountProvider with the provided parameters.
+// It initializes the Ethereum client, owner's address, and the smart account factory contract.
+func NewSmartAccountProvider(params SmartAccountProviderParams) (*SmartAccountProvider, error) {
+	client, err := createEthClient(params.RPC)
+	if err != nil {
+		return nil, err
+	}
+
+	owner, err := privateKeyToAddress(params.OwnerPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	fac, err := factory.NewFactory(common.HexToAddress(params.SmartAccountFactoryAddress), client)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SmartAccountProvider{
+		Client:    client,
+		Owner:     owner,
+		SAFactory: fac,
+	}, nil
+}
+
+// createEthClient connects to an Ethereum node via the specified RPC endpoint
 // and returns an Ethereum client. It panics on connection errors.
-func CreateClient(rpc string) *ethclient.Client {
+func createEthClient(rpc string) (*ethclient.Client, error) {
 	cl, err := ethclient.Dial(rpc)
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return cl
+	return cl, nil
 }
 
-// PrivateKeyToAddress converts a private key (in hexadecimal format) to an Ethereum address.
-func PrivateKeyToAddress(pvtKey string) common.Address {
-	privateKey, err := crypto.HexToECDSA(pvtKey)
+// privateKeyToAddress converts a private key (in hexadecimal format) to an Ethereum address.
+func privateKeyToAddress(privateKey string) (common.Address, error) {
+	key, err := crypto.HexToECDSA(privateKey)
+
 	if err != nil {
-		panic(err)
+		return common.Address{}, err
 	}
 
-	publicKey := privateKey.Public()
+	return crypto.PubkeyToAddress(key.PublicKey), nil
+}
 
-	// Check if the public key can be type-asserted to *ecdsa.PublicKey.
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		panic("error casting public key to ECDSA")
+// GetSmartAccountAddress retrieves the address of a smart account based on a given salt value.
+func (sap *SmartAccountProvider) GetSmartAccountAddress(salt int64) (common.Address, error) {
+
+	address, err := sap.SAFactory.GetAddress(nil, sap.Owner, big.NewInt(salt))
+	if err != nil {
+		return common.Address{}, err
 	}
 
-	// Convert the ECDSA public key to an Ethereum address.
-	address := crypto.PubkeyToAddress(*publicKeyECDSA)
-
-	return address
+	return address, nil
 }
