d9133c6827fb62bcc4d3ad210f9dc501d25f75ef
pavanleton <pavankumarkpkp@gmail.com>
2023-10-12 14:24:17 +0530

    feat(send-userop): develop the payload to send a user operation transaction

--- example/main.go
+++ example/main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/pavankpdev/goaa"
+	"math/big"
 )
 
 func main() {
 
-	const RPC = "<RPC>"
+	const RPC = "https://polygon-mumbai.g.alchemy.com/v2/u-FhnHbTFL8OASxmdclXSWKS-YcypJzH"
 	const SmartAccountFactoryAddress = "0x9406Cc6185a346906296840746125a0E44976454" // https://docs.alchemy.com/docs/creating-a-smart-contract-account-and-sending-userops#1b-get-constants
-	const PrivateKey = "<PrivateKey>"
+	const PrivateKey = "0x1934c4fa3a8c7130c55b4b2933657b584102c02e6fdc682394728822a714404e"
 
-	params := goaa.SmartAccountProviderParams{
+	eth := big.NewFloat(0.1)
+	wei := new(big.Float)
+	wei.Mul(eth, big.NewFloat(params.Ether))
+
+	SAParams := goaa.SmartAccountProviderParams{
 		OwnerPrivateKey:            PrivateKey,
 		RPC:                        RPC,
-		EntryPointAddress:          "",
+		EntryPointAddress:          "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
 		SmartAccountFactoryAddress: SmartAccountFactoryAddress,
 	}
 
-	client, err := goaa.NewSmartAccountProvider(params)
+	client, err := goaa.NewSmartAccountProvider(SAParams)
 
 	if err != nil {
+		fmt.Println("Error creating SmartAccountProvider:", err)
 		panic(err)
 	}
 
@@ -29,5 +36,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("My samrt account address is %v\n", address)
+
+	nonce, err := client.SendUserOpsTransaction(goaa.TargetParams{
+		Target: "0x94f3178AcB40d0E9c6967108e3711CF047D3240A",
+		Data:   "0x",
+		Value:  wei.String(),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("My samrt account address is %v\n", nonce)
 
 }
--- goaa.go
+++ goaa.go
@@ -1,11 +1,21 @@
 package goaa
 
 import (
+	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	entrypoint "github.com/pavankpdev/goaa/gen"
 	factory "github.com/pavankpdev/goaa/gen"
+	"io"
 	"math/big"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 // SmartAccountProviderParams stores the parameters required to initialize the SmartAccountProvider.
@@ -18,9 +28,38 @@ type SmartAccountProviderParams struct {
 
 // SmartAccountProvider is a struct that manages interaction with Ethereum smart contracts.
 type SmartAccountProvider struct {
-	Client    *ethclient.Client // Ethereum client for interacting with the blockchain
-	Owner     common.Address    // Ethereum address of the owner
-	SAFactory *factory.Factory  // Smart account factory contract instance
+	Client      *ethclient.Client      // Ethereum client for interacting with the blockchain
+	Owner       common.Address         // Ethereum address of the owner
+	SAFactory   *factory.Factory       // Smart account factory contract instance
+	EntryPoint  *entrypoint.EntryPoint // Smart account factory contract instance
+	SignMessage func(message string) (string, error)
+}
+
+type TargetParams struct {
+	Target string
+	Data   string
+	Value  string
+}
+
+type UOps struct {
+	Sender               string `json:"sender"`
+	Nonce                string `json:"nonce"`
+	InitCode             string `json:"initCode"`
+	CallData             string `json:"callData"`
+	Signature            string `json:"signature"`
+	CallGasLimit         string `json:"callGasLimit"`
+	VerificationGasLimit string `json:"verificationGasLimit"`
+	PreVerificationGas   string `json:"preVerificationGas"`
+	MaxFeePerGas         string `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas string `json:"maxPriorityFeePerGas"`
+	PaymasterAndData     string `json:"paymasterAndData"`
+}
+
+type UserOperationTxnPayload struct {
+	Id      int64  `json:"id"`
+	Jsonrpc string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	Params  []any  `json:"params"`
 }
 
 // NewSmartAccountProvider creates a new instance of SmartAccountProvider with the provided parameters.
@@ -31,7 +70,7 @@ func NewSmartAccountProvider(params SmartAccountProviderParams) (*SmartAccountPr
 		return nil, err
 	}
 
-	owner, err := privateKeyToAddress(params.OwnerPrivateKey)
+	owner, err := privateKeyToAddress(params.OwnerPrivateKey[2:])
 	if err != nil {
 		return nil, err
 	}
@@ -41,11 +80,43 @@ func NewSmartAccountProvider(params SmartAccountProviderParams) (*SmartAccountPr
 		return nil, err
 	}
 
+	ep, err := entrypoint.NewEntryPoint(common.HexToAddress(params.EntryPointAddress), client)
+	if err != nil {
+		return nil, err
+	}
+
 	return &SmartAccountProvider{
-		Client:    client,
-		Owner:     owner,
-		SAFactory: fac,
+		Client:     client,
+		Owner:      owner,
+		SAFactory:  fac,
+		EntryPoint: ep,
+		SignMessage: func(message string) (string, error) {
+			signature, err := _signMessage(message, params.OwnerPrivateKey)
+			if err != nil {
+				return "", err
+			}
+
+			return signature, nil
+		},
 	}, nil
+}
+
+func _signMessage(dataToSign string, privateKeyHex string) (string, error) {
+	privateKey, err := crypto.HexToECDSA(privateKeyHex[2:])
+	if err != nil {
+		return "", err
+	}
+
+	data := []byte(dataToSign)
+	hash := crypto.Keccak256Hash(data)
+
+	signature, err := crypto.Sign(hash.Bytes(), privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	return hexutil.Encode(signature), nil
+
 }
 
 // createEthClient connects to an Ethereum node via the specified RPC endpoint
@@ -80,4 +151,86 @@ func (sap *SmartAccountProvider) GetSmartAccountAddress(salt int64) (common.Addr
 	}
 
 	return address, nil
+}
+
+func (sap *SmartAccountProvider) SendUserOpsTransaction(target TargetParams) (any, error) {
+	// Nonce
+	nonce, err := sap.Client.PendingNonceAt(context.Background(), sap.Owner)
+	if err != nil {
+		return 0, err
+	}
+
+	sender, err := sap.GetSmartAccountAddress(int64(nonce))
+	if err != nil {
+		return 0, err
+	}
+
+	fmt.Println(sender)
+
+	jsonStr, err := json.Marshal(target)
+	if err != nil {
+		return 0, err
+	}
+
+	calldata := []byte(jsonStr)
+
+	signature, err := sap.SignMessage("Create Account")
+	if err != nil {
+		return 0, err
+	}
+
+	nonceInHex := strconv.FormatInt(int64(nonce), 16)
+
+	uo := UOps{
+		Sender:               "0xCeFc6b95c885D17E3c328f57F24b13E3EE82Aec2",
+		Nonce:                "0x" + nonceInHex,
+		InitCode:             "0x",
+		CallData:             "0x" + hex.EncodeToString(calldata),
+		Signature:            signature,
+		CallGasLimit:         "0x2710",
+		VerificationGasLimit: "0x2710",
+		PreVerificationGas:   "0x402db0",
+		MaxFeePerGas:         "0x17190c894e",
+		MaxPriorityFeePerGas: "0x3812ed1a0",
+		PaymasterAndData:     "0x",
+	}
+
+	var uoArray []any
+	uoArray = append(uoArray, uo)
+	uoArray = append(uoArray, "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789")
+	fmt.Println(uoArray)
+
+	bodyPayload, err := json.Marshal(UserOperationTxnPayload{
+		Id:      1,
+		Jsonrpc: "2.0",
+		Method:  "eth_sendUserOperation",
+		Params:  uoArray,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	fmt.Println(string(bodyPayload))
+
+	//res, err := sap.EntryPoint.HandleOps(nil, uoArray, sap.Owner)
+
+	url := "https://polygon-mumbai.g.alchemy.com/v2/u-FhnHbTFL8OASxmdclXSWKS-YcypJzH"
+
+	payload := strings.NewReader(string(bodyPayload))
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("content-type", "application/json")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	return string(body), nil
+
+	// Create target bytes
+
+	// generate UO object
 }
